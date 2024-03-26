2e26242ef0eb7e1a5f4c6376d722a9f4b5bd2931
Md. Farhan Jahin <farhan15-13948@diu.edu.bd>
2024-03-26 19:29:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <div className=' bg-orange-400'>
+      <h4 className='text-4xl '>React Knowledge cafe</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
