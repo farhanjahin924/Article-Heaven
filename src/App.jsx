ef44731b1be9a4adac88a33c96f5832b7cb92b7f
Md. Farhan Jahin <farhan15-13948@diu.edu.bd>
2024-03-26 20:46:55 +0600

    web name update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       
       <div className=' bg-orange-400'>
-      <h4 className='text-4xl '>React Knowledge cafe</h4>
+      <h4 className='text-4xl text-center text-white '>Article Haven</h4>
       </div>
     </>
   )
